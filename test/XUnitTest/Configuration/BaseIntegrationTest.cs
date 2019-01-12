2555c725ef57e66d1cb265e7124170ebcb04bb8f
Tiago Rodrigues <tiago.r.p@hotmail.com>
2019-01-12 20:21:07 -0200

    UnitTeste implementado classes de integração

---
+++ test/XUnitTest/Configuration/BaseIntegrationTest.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.TestHost;
+using Microsoft.EntityFrameworkCore;
+using System.Net.Http;
+using System.Threading.Tasks;
+using WorkshopAspnetCore.Data;
+using Xunit;
+
+namespace XUnitTest.Configuration
+{
+    [Collection("Base Collection")]
+    public abstract class BaseIntegrationTest
+    {
+        protected BaseIntegrationTest(BaseTestFixture fixture)
+        {
+            Fixture = fixture;
+
+            TestDataContext = Fixture.TestDataContext;
+            Server = Fixture.Server;
+            Client = Fixture.Client;
+
+            ClearDb().Wait();
+        }
+
+        protected readonly TestServer Server;
+        protected readonly HttpClient Client;
+        protected readonly DataContext TestDataContext;
+
+        protected BaseTestFixture Fixture { get; }
+
+        private async Task ClearDb()
+        {
+            var commands = new[]
+            {
+                "EXEC sp_MSForEachTable 'ALTER TABLE ? NOCHECK CONSTRAINT ALL'",
+                "EXEC sp_MSForEachTable 'DELETE FROM ?'",
+                "EXEC sp_MSForEachTable 'ALTER TABLE ? CHECK CONSTRAINT ALL'"
+            };
+
+            await TestDataContext.Database.OpenConnectionAsync();
+
+            foreach (var command in commands)
+            {
+                await TestDataContext.Database.ExecuteSqlCommandAsync(command);
+            }
+
+            TestDataContext.Database.CloseConnection();
+        }
+
+    }
+}
---
+++ test/XUnitTest/Configuration/BaseTestFixture.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Net.Http;
+using WorkshopAspnetCore;
+using WorkshopAspnetCore.Data;
+
+namespace XUnitTest.Configuration
+{
+    public class BaseTestFixture : IDisposable
+    {
+        public readonly TestServer Server;
+        public readonly HttpClient Client;
+        public readonly DataContext TestDataContext;
+        public readonly IConfigurationRoot Configuration;
+
+        public BaseTestFixture()
+        {
+            var envName = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIROMENT");
+
+            var builder = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddJsonFile("appsettings.{envName}.json", optional: true)
+                .AddEnvironmentVariables();
+
+            Configuration = builder.Build();
+
+            var opts = new DbContextOptionsBuilder<DataContext>();
+            opts.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"));
+            TestDataContext = new DataContext(opts.Options);
+            SetupDataBase();
+
+            Server = new TestServer(new WebHostBuilder().UseStartup<Startup>());
+            Client = Server.CreateClient();
+        }
+
+        private void SetupDataBase()
+        {
+            try
+            {
+                TestDataContext.Database.EnsureCreated();
+                TestDataContext.Database.Migrate();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public void Dispose()
+        {
+            TestDataContext.Dispose();
+            Server.Dispose();
+            Client.Dispose();
+        }
+    }
+}
